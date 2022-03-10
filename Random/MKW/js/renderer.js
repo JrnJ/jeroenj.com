3661ff317d012e086f75429815b8949de9db0531
JrnJ <jeroenjochems@outlook.com>
2022-03-10 20:16:59 +0100

    Working Graph

--- Random/MKW/js/renderer.js
+++ Random/MKW/js/renderer.js
@@ -30,7 +30,7 @@ function ItemNameToColor(name) {
         case "RedShell":
             return "#F90018";
         case "TripleRedShells":
-            return "#FFFF00";
+            return "#FF0000";
 
         case "Banana":
             return "#FFFF00";
@@ -39,15 +39,33 @@ function ItemNameToColor(name) {
 
         case "FakeItemBox":
             return "#4a0000";
+        case "ThunderCloud":
+            return "#7894CF";
 
         case "Mushroom":
             return "#ff2626";
         case "TripleMushrooms":
             return "#ffff00";
         case "GoldenMushroom":
-            return "#";
+            return "#ffa508";
         case "MegaMushroom":
-            return "#";
+            return "#d6a508";
+
+        case "Bob-omb":
+            return "#26273D";
+        case "BlueShell":
+            return "#0873ff";
+        case "POWBlock":
+            return "#313a84";
+        case "Blooper":
+            return "#525a63"; 
+        case "Shock":
+            return "#FFFF00";
+
+        case "Star":
+            return "#ffff63";
+        case "BulletBill":
+            return "#000000";
 
         default:
             return 'purple';
@@ -58,19 +76,19 @@ async function Main() {
     // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
 
     //await GetJSON(); // stupid cors
-    let json = JSON.parse('[{"Racers":12,"Positions":[{"Position":1,"ItemProbability":[{"ItemName":"GreenShell","Probability":32.5},{"ItemName":"Banana","Probability":37.5},{"ItemName":"FakeItemBox","Probability":20},{"ItemName":"TripleBananas","Probability":10}]},{"Position":2,"ItemProbability":[{"ItemName":"GreenShell","Probability":17.5},{"ItemName":"RedShell","Probability":25},{"ItemName":"Banana","Probability":20},{"ItemName":"FakeItemBox","Probability":7.5},{"ItemName":"Mushroom","Probability":12.5},{"ItemName":"TripleGreenShells","Probability":5},{"ItemName":"TripleBananas","Probability":12.5}]}]}]');
+    let json = JSON.parse('[{"Racers":12,"Positions":[{"Position":1,"ItemProbability":[{"Name":"GreenShell","Probability":32.5},{"Name":"Banana","Probability":37.5},{"Name":"FakeItemBox","Probability":20},{"Name":"TripleBananas","Probability":10}]},{"Position":2,"ItemProbability":[{"Name":"GreenShell","Probability":17.5},{"Name":"RedShell","Probability":25},{"Name":"Banana","Probability":20},{"Name":"FakeItemBox","Probability":7.5},{"Name":"Mushroom","Probability":12.5},{"Name":"TripleGreenShells","Probability":5},{"Name":"TripleBananas","Probability":12.5}]},{"Position":3,"ItemProbability":[{"Name":"GreenShell","Probability":15},{"Name":"RedShell","Probability":25},{"Name":"Banana","Probability":7.5},{"Name":"FakeItemBox","Probability":5},{"Name":"Mushroom","Probability":17.5},{"Name":"Bob-omb","Probability":2.5},{"Name":"ThunderCloud","Probability":2.5},{"Name":"TripleGreenShells","Probability":10},{"Name":"TripleRedShells","Probability":5},{"Name":"TripleBananas","Probability":10}]}]}]');
     console.log(json);
 
-    // for (let racers = 11; racers >= 0; racers--) {
-    //     //console.log("Amount of racers: " + (racers + 1));
+    for (let racers = 11; racers >= 0; racers--) {
+        //console.log("Amount of racers: " + (racers + 1));
 
-    //     for (let positions = 0; positions <= racers; positions++)
-    //     {
-    //         //console.log("Positions: " + (positions + 1));
-    //     }
-    // }
+        for (let positions = 0; positions <= racers; positions++)
+        {
+            //console.log("Positions: " + (positions + 1));
+        }
+    }
 
-    // Get Racers Amount
+    //Get Racers Amount
     for (let racers = json.length; racers > 0; racers--)
     {
         let placement = json[racers - 1];
@@ -90,7 +108,26 @@ async function Main() {
 
                 const height = canvasHeight / 100 * item.Probability;
 
-                DrawQuad(new Vector2(100 * positions + (positions * 10), previousHeight), new Vector2(100, height), ItemNameToColor(item.Name));
+                DrawQuad(
+                    new Vector2(100 * positions + (positions * 10), previousHeight), new Vector2(100, height), 
+                    ItemNameToColor(item.Name));
+
+                // Make sure to crop the image
+                // Sorry for double code but its faster
+                if (height < 60)
+                {
+                    await DrawImage("./images/Items/" + item.Name + ".png",
+                        new Vector2(100 * positions + (positions * 10) + 20, previousHeight - 30 + height / 2 + ((60 - height) / 2)), new Vector2(60, 60),
+                        new Vector2(0, (60 - height) / 2), new Vector2(60, height)
+                    );
+                }
+                else
+                {
+                    await DrawImage("./images/Items/" + item.Name + ".png",
+                        new Vector2(100 * positions + (positions * 10) + 20, previousHeight - 30 + height / 2), new Vector2(60, 60),
+                        new Vector2(0, 0), new Vector2(60, 60)
+                    );
+                }
 
                 previousHeight += height;
             }
@@ -122,10 +159,6 @@ const DrawText = (text, position, color, verticalCenter = true) => {
     ctx.fillText(text, position.x, position.y + 4);
 }
 
-/// Draws a line on the canvas
-// p1 > Point 1
-// p2 > Point 2
-// color > HEX color line
 const DrawLine = (p1, p2, color) => {
     ctx.beginPath();
 
@@ -164,6 +197,29 @@ const DrawCircle = (position, radius, fill, color) => {
         console.log("Hahhaa");
         ctx.stroke();
     }
+}
+
+async function DrawImage(image, leftTop, size, sourceLeftTop, sourceSize) {
+    //ctx.beginPath();
+
+    let img = new Image();
+
+    await new Promise(r => img.onload = r, img.src = image);
+    ctx.drawImage(img, 
+        // Where to place  
+        img.naturalHeight / size.x * sourceLeftTop.x,
+
+        // Size
+        img.naturalWidth  / size.y * sourceLeftTop.y,
+
+
+        // Where to start   
+        img.naturalHeight / size.x * sourceSize.x,
+
+        // Where to end
+        img.naturalWidth  / size.y * sourceSize.y,
+        
+        leftTop.x, leftTop.y, sourceSize.x, sourceSize.y);
 }
 
 window.onload = Main;
